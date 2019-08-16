1060658f2cb7a7c89dc645ad1077c7ccf5b48952
root <2710004119@qq.com>
2019-08-16 18:24:13 +0800

    create project

---
+++ src/main/java/com/gem/CommunityprojectApplication.java
@@ -0,0 +1,13 @@
+package com.gem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityprojectApplication.class, args);
+    }
+
+}
